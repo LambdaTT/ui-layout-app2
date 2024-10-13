5793a24c232b76dde70eda9157cd8e93c8c13124
Gabriel Valentoni Guelfi <gabriel.valguelfi@gmail.com>
2024-10-13 14:22:49 -0300

    Completo e utilizável

---
+++ Page.vue
@@ -0,0 +1,51 @@
+<template>
+  <q-page class="flex q-pa-sm text-center">
+    <div animated>
+      <q-card class="my-card" flat bordered>
+        <q-card-section v-if="!hidePageHeader">
+          <PageHeader :pageTitle="PageTitle" :breadcrumb="Breadcrumb" />
+        </q-card-section>
+
+        <q-card-section class="text-justify">
+          <slot></slot>
+        </q-card-section>
+
+        <!--Secondary Sections-->
+        <div v-for="(section, name) in $slots" :key="name">
+          <q-separator v-if="name != 'actions' && name != 'default'"></q-separator>
+          <q-card-section v-if="name != 'actions' && name != 'default'">
+            <slot :name="name"></slot>
+          </q-card-section>
+        </div>
+      </q-card>
+    </div>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'ui-layoutapp2-page',
+
+  props: {
+    HidePageHeader: Boolean,
+    PageTitle: String,
+    Breadcrumb: Array
+  },
+
+  data() {
+    return {
+      hidePageHeader: true
+    }
+  },
+
+  mounted() {
+    this.hidePageHeader = this.HidePageHeader;
+  }
+}
+</script>
+
+<style scoped>
+.q-page {
+  display: block;
+}
+</style>
